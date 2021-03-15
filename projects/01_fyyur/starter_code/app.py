9a2de613ced67d5f30fc686390478499f8444fb0
xiao <xiao.dalod@gmail.com>
2021-03-15 15:50:49 +0100

    Added del venue, return to venues after deletion

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -5,7 +5,7 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -291,8 +291,7 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+
   try:
     venue = Venue.query.get(venue_id)
     genres = Genre.query.filter(Genre.venue_id == venue_id)
@@ -300,15 +299,15 @@ def delete_venue(venue_id):
       db.session.delete(genre)
     db.session.delete(venue)
     db.session.commit()
+    flash('Venue ' + venue.name + ' was successfully deleted!')
   except:
     db.session.rollback()
     print(sys.exc_info())
+    flash('An error occurred. Venue ' + venue.name + ' could not be deleted.')
   finally:
     db.session.close()
 
-  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-  # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  return jsonify({'success': True})
 
 #  Artists
 #  ----------------------------------------------------------------
